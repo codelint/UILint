93153323bf4287c5ef960d71564d9bd3e14f27d3
codelint <codelint@foxmail.com>
2024-07-07 22:35:15 +0800

    enhance

--- Sources/UILint/Extension/Color.swift
+++ Sources/UILint/Extension/Color.swift
@@ -35,6 +35,35 @@ public extension Color {
         )
     }
     
+    init(hex: Int, alpha: Double = 1) {
+        let components = hex > 0x00FFFFFF ? (
+            R: Double((hex >> 24) & 0xff) / 255,
+            G: Double((hex >> 16) & 0xff) / 255,
+            B: Double((hex >> 08) & 0xff) / 255,
+            A: Double((hex >> 00) & 0xff) / 255
+        ) : (
+            R: Double((hex >> 16) & 0xff) / 255,
+            G: Double((hex >> 08) & 0xff) / 255,
+            B: Double((hex >> 00) & 0xff) / 255,
+            A: alpha
+        )
+        self.init(
+            .sRGB,
+            red: components.R,
+            green: components.G,
+            blue: components.B,
+            opacity: alpha
+        )
+    }
+    
+    static func random() -> Color {
+        Color(
+            red: Double(arc4random()) / Double(UInt32.max),
+            green: Double(arc4random()) / Double(UInt32.max),
+            blue: Double(arc4random()) / Double(UInt32.max)
+        )
+    }
+    
 }
 
 #if canImport(UIKit)
